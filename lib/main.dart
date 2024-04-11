1ccb3ef12e9e887c362bf9a686639d4c6f861f5a
craftidev <ivann.codev@gmail.com>
2024-04-11 12:42:53 +0200

    create and implement MainGui with TheData and color scheme custom package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clarity/src/rust/api/simple.dart';
 import 'package:clarity/src/rust/frb_generated.dart';
+import 'package:clarity/themes/app_colors.dart';
 
 Future<void> main() async {
   await RustLib.init();
@@ -13,11 +14,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+      title: 'clarity',
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.blackColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.blackColor,
+        ),
+      ),
+      home: const MainGui(),
+    );
+  }
+}
+
+class MainGui extends StatelessWidget {
+  const MainGui({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 20,
+        title: Text(
+          greet(name: "user"),
+          style: const TextStyle(color: Colors.white, fontSize: 18),
+        )
+      ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(5),
+          color: AppColors.backgroundColor,
         ),
       ),
     );
