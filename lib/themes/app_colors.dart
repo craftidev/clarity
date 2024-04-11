2b3d34c7811e919c1d22827e3964caaef0cf4b76
craftidev <ivann.codev@gmail.com>
2024-04-11 12:42:06 +0200

    more colors

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const backgroundColor = Color.fromARGB(255, 27, 15, 24);
   static const lightSquareColor = Color.fromARGB(255, 156, 137, 152);
   static const darkSquareColor = Color.fromARGB(255, 70, 58, 67);
+  static const primaryColor = Color.fromARGB(255, 148, 92, 134);
 }
