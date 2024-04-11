b31e735e0ec4711a713b2fdbd19e3fbfcb242685
craftidev <ivann.codev@gmail.com>
2024-04-11 14:05:11 +0200

    better color name

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const blackColor =  Colors.black;
-  static const backgroundColor = Color.fromARGB(255, 27, 15, 24);
-  static const lightSquareColor = Color.fromARGB(255, 156, 137, 152);
-  static const darkSquareColor = Color.fromARGB(255, 70, 58, 67);
-  static const primaryColor = Color.fromARGB(255, 148, 92, 134);
+  static const voidColor =  Colors.black;
+  static const background = Color.fromARGB(255, 27, 15, 24);
+  static const lightSquare = Color.fromARGB(255, 156, 137, 152);
+  static const darkSquare = Color.fromARGB(255, 70, 58, 67);
+  static const primary = Color.fromARGB(255, 148, 92, 134);
 }
