9a2991ccc4541922ed4b835628ad8909b86050da
craftidev <ivann.codev@gmail.com>
2024-04-11 10:13:54 +0200

    color theme

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blackColor =  Colors.black;
+  static const backgroundColor = Color.fromARGB(255, 27, 15, 24);
+  static const lightSquareColor = Color.fromARGB(255, 156, 137, 152);
+  static const darkSquareColor = Color.fromARGB(255, 70, 58, 67);
+}
